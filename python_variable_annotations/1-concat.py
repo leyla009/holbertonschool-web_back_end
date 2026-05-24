fec01ac4be5664d3a80d3727a300f7832802f9fb
Leyla <Leyla.khaspolad@gmail.com>
2026-05-24 18:01:51 +0000

    Add task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to provide a type-annotated concatenation function."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenated result of two strings."""
+    return str1 + str2
