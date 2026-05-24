1d124b0455c72b7d200daf6fe30df634c054c388
Leyla <Leyla.khaspolad@gmail.com>
2026-05-24 18:04:19 +0000

    Add task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to provide a type-annotated function to convert float to string."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float n."""
+    return str(n)
