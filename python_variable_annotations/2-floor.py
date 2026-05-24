3313aeb448cef90eee60736dde2bde6d73d11356
Leyla <Leyla.khaspolad@gmail.com>
2026-05-24 18:02:59 +0000

    Add task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to provide a type-annotated floor function."""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float n."""
+    return math.floor(n)
