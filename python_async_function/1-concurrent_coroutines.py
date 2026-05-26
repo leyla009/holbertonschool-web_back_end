9eb7cfd1f308985037d6bc6829aad88110d3344f
Leyla <Leyla.khaspolad@gmail.com>
2026-05-26 15:48:41 +0200

    Fix task 1 issues

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -13,6 +13,11 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Spawns wait_random n times with the specified max_delay.
     Returns the list of all the delays in ascending order.
     """
+    # Create a list of task objects
     tasks = [wait_random(max_delay) for _ in range(n)]
+    
+    # Gather them to run concurrently and wait for all to complete
     delays = await asyncio.gather(*tasks)
+    
+    # Returning the result of gather, which is already a list
     return list(delays)
