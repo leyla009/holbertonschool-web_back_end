be37e8e34d3e9330a631636c14b77ad9d9796eab
Leyla <Leyla.khaspolad@gmail.com>
2026-07-21 12:11:45 +0000

    Add task 4 solution for Node JS basic (small HTTP server)

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+/**
+ * Creates a small HTTP server using Node's http module.
+ */
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
